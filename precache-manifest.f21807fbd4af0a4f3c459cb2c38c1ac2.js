d2a647b8de42f2d77abddbf113de8d17661ed935
Trung Ha <ktrungha@hotmail.com>
2018-12-08 10:18:51 +0700

    initial deploy

---
+++ precache-manifest.f21807fbd4af0a4f3c459cb2c38c1ac2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a1f4859434a3751df8d4",
+    "url": "/static/js/main.a1f48594.chunk.js"
+  },
+  {
+    "revision": "7f61433a59577cd27a55",
+    "url": "/static/js/1.7f61433a.chunk.js"
+  },
+  {
+    "revision": "a1f4859434a3751df8d4",
+    "url": "/static/css/main.e59d3472.chunk.css"
+  },
+  {
+    "revision": "01f87b9c0dc5fb692fe4ddd84eb5df56",
+    "url": "/index.html"
+  }
+];
